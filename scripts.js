26e562870282aa8b37db00e6058eef18eedc2e61
EmmetMack <emmet.mack10@gmail.com>
2022-02-24 17:08:22 -0600

    Create function that compares player and computer selection for game round

--- scripts.js
+++ scripts.js
@@ -12,3 +12,27 @@ function computerPlay() {
     console.log(choice);
     return choice;
 }
+
+//play a round of rock paper scissors
+function playRound(playerSelection, computerSelection) {
+    let cleansedPlayer = playerSelection.toLowerCase();
+    if (cleansedPlayer === "rock" && computerSelection === "scissors") {
+        return ("You win! Rock beats Scissors")
+    } else if (cleansedPlayer === "paper" && computerSelection === "rock") {
+        return ("You win! Paper beats Rock");
+    } else if (cleansedPlayer === "scissors" && computerSelection === "paper") {
+        return ("You win! Scissors beats paper");
+    } else if (computerSelection === "rock" && cleansedPlayer === "scissors") {
+        return ("You lose! Rock beats Scissors")
+    } else if (computerSelection === "paper" && cleansedPlayer === "rock") {
+        return ("You lose! Paper beats Rock");
+    } else if (computerSelection === "scissors" && cleansedPlayer === "paper") {
+        return ("You lose! Scissors beats paper");
+    } else {
+        return ("DRAW! You both put: ", cleansedPlayer);
+    }
+}
+
+// const playerSelection = 'rock';
+// const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection));
